6cefa0ac09ce6172062959123823690aa5224dc8
mikha1lov <mihailov.alex.2011@gmail.com>
2019-11-18 21:46:17 +0300

    Add accepting/declining meetings methods

--- meeting/views.py
+++ meeting/views.py
@@ -1,4 +1,6 @@
-from rest_framework import permissions, mixins
+from rest_framework import permissions, mixins, status, serializers
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
 from .models import Meeting, MeetingStatus
@@ -6,7 +8,7 @@ from .serializers import MeetingSerializer
 
 
 class MeetingViewSet(mixins.ListModelMixin, GenericViewSet):
-    http_method_names = ['get', ]
+    http_method_names = ['get', 'post']
     serializer_class = MeetingSerializer
     permission_classes = [permissions.IsAuthenticated, ]
     queryset = Meeting.objects.all()
@@ -16,3 +18,21 @@ class MeetingViewSet(mixins.ListModelMixin, GenericViewSet):
             users=self.request.user).exclude(
             statuses__status=MeetingStatus.DECLINED
         )
+
+    @action(methods=['post', ], detail=True, serializer_class=serializers.Serializer,
+            url_path='accept', permission_classes=[permissions.IsAuthenticated])
+    def accept_meeting(self, request, **kwargs):
+        self._update_meeting_status(MeetingStatus.ACCEPTED)
+        return Response(status=status.HTTP_200_OK)
+
+    @action(methods=['post', ], detail=True, serializer_class=serializers.Serializer,
+            url_path='decline', permission_classes=[permissions.IsAuthenticated])
+    def decline_meeting(self, request, **kwargs):
+        self._update_meeting_status(MeetingStatus.DECLINED)
+        return Response(status=status.HTTP_200_OK)
+
+    def _update_meeting_status(self, new_status):
+        meeting = self.get_object()
+        meeting_status = meeting.statuses.get(user=self.request.user)
+        meeting_status.status = new_status
+        meeting_status.save()
